608229b59ae89bdb4763e086d99179a9e1106944
Costas <gryxitl@gmail.com>
2023-02-04 18:52:18 -0500

    Almost got inventory bottom panel working

--- CurrentItems/InventoryPanel.gd
+++ CurrentItems/InventoryPanel.gd
@@ -2,12 +2,16 @@ extends Control
 
 # Declare member variables here. Examples:
 var ingredientList = []
-var maxIngredients = 10
+export var maxIngredients = 10
+var ingredientsToRemove = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	#pass # Replace with function body.
+	
+	#if ingredientsList is empty, add a random ingredient
+	if len(ingredientList) == 0:
+		addIngredients([InventoryIngredient.new("cheese",3),InventoryIngredient.new("egg",4),InventoryIngredient.new("bread",5)])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -25,3 +29,24 @@ func addIngredients(ingredients):
 	if len(ingredientList) > maxIngredients:
 		for i in range(len(ingredients)):
 			ingredientList.pop_front()
+
+#This function ages the ingredients in the list and removes them if they are too old
+func ageIngredients():
+	for i in range(len(ingredientList)):
+		ingredientList[i].ageIngredient
+		
+		#if the ingredient is too old, add it to the list of ingredients to remove
+		if ingredientList[i].age <= 0:
+			ingredientsToRemove.append(ingredientList[i])
+	
+	#remove the ingredients from the list
+	if ingredientsToRemove.size() > 0:
+		for i in range(len(ingredientsToRemove)):
+			ingredientList.ingredientsToRemove[i].queue_free()
+			
+#Generate a button for an ingredient
+func generateIngredientButton(ingredient):
+	var button = Button.new()
+	button.text = ingredient.name
+	button.connect("pressed", self, "onIngredientButtonPressed", [ingredient])
+	return button
