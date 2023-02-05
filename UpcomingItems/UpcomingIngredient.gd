4f92c22810d03743681a59e68b0a584bc83d33d4
Carpinch0s <rgirado@gmail.com>
2023-02-05 11:31:21 -0500

    restarting all of it under new organization

--- UpcomingItems/UpcomingIngredient.gd
+++ UpcomingItems/UpcomingIngredient.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+export var ingredient = ""
+$AnimatedSprite.animation = ingredient
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- UpcomingItems/UpcomingIngredientPanel.gd
+++ UpcomingItems/UpcomingIngredientPanel.gd
@@ -1,8 +1,38 @@
-extends Control
+extends Viewport
+
+# holds an array of arrays.
+# after each turn we remove the top array, we push all other arrays by one, and we generate a new one.
+
+# ABC
+# CBA
+# A?A
+# BC?
+var newLine = ["banana","banana","banana"]
+var lineOne = ["banana","tomato","milk"]
+var lineTwo = ["flour","sugar","mushroom"]
+var lineThree = ["banana","meat","egg"]
+var lineFour = ["egg","banana","flour"]
+
+func updateUpcomingIngredients ():
+	lineFour.clear
+	lineFour.append_array(lineThree)
+	lineThree.clear
+	lineThree.append_array(lineFour)
+	lineFour.clear
+	lineFour.append_array(newLine)
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
 func addIngredients(_ingredients):
 	pass
