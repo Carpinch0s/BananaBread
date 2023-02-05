28abd609a53499f0d0c41a9a4034f9a466b3f8dd
Costas <gryxitl@gmail.com>
2023-02-05 13:33:24 -0500

    Selecting objects is visual now and might actually add objects to the group

--- CurrentItems/IngredientCard.gd
+++ CurrentItems/IngredientCard.gd
@@ -3,6 +3,7 @@ extends Control
 var ingredientName
 var img
 var expiration
+var selected = false
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -17,3 +18,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_IngredientCard_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == BUTTON_LEFT:
+			toggleSelected()
+
+func toggleSelected():
+	#If the object is selected remove it from the selected group
+	if selected:
+		selected = false
+		get_node("Image").color = Color(0.257813, 0.240692, 0.223572)
+		remove_from_group("selected")
+	#If the object is not selected add it to the selected group
+	else:
+		selected = true
+		get_node("Image").color = Color(0.556055, 0.570313, 0.499023)
+		add_to_group("selected")
