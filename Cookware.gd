bd6cfe9e702859dd58dee73fa9af9e4a0604223d
TheOneKnownSoldier <theoneknownsoldier@gmail.com>
2023-02-04 20:56:56 -0500

    Cookware modifications. Adding them to the main screen.

--- Cookware.gd
+++ Cookware.gd
@@ -1,16 +1,16 @@
 extends Area2D
 
+signal CookwarePressed(id)
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+export var Type = "Saucepan"
+export var BoxColor = Color(100,100,0,1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Label.text = Type
+	$ColorRect.color = BoxColor
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_ColorRect_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == BUTTON_LEFT:
+			emit_signal('CookwarePressed', Type)
--- Main.gd
+++ Main.gd
@@ -1,5 +1,6 @@
 extends Node
 
+export(PackedScene) var cookwareScene
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -19,4 +20,14 @@ func _ready():
 	
 	var inventoryData = $RecipeManager.generateNewIngredients(3)
 	$InventoryPanel.addIngredients(inventoryData)
+	
+
+
+func _on_CookwarePressed(id):
+	var string = "clicked on " + id
+	print(string)
+	var completedItem = cookwareScene.instance()
+	completedItem.position = Vector2(10,10)
+	completedItem.Type = "Grill"
+	add_child(completedItem)
 	
